53a11aa3f7cd1b0a75150688bc285a731f69f35f
Seunggyu Park <109791533+Seung-gyuu@users.noreply.github.com>
2024-03-29 06:28:42 -0600

    Update and rename 002 Min&MaxVals to 002 Min&MaxVals.py
    updated contents

--- Programmers/002 Min&MaxVals.py
+++ Programmers/002 Min&MaxVals.py
@@ -1,5 +1,5 @@
-#Complete the function solution that returns a string in the form "(minimum value) (maximum value)", where s contains numbers separated by spaces. 
-#For example, if s is "1 2 3 4", it should return "1 4", and if s is "-1 -2 -3 -4", it should return "-4 -1".
+# Complete the function solution that returns a string in the form "(minimum value) (maximum value)", where s contains numbers separated by spaces. 
+# For example, if s is "1 2 3 4", it should return "1 4", and if s is "-1 -2 -3 -4", it should return "-4 -1".
 
 def solution(s):
     num_list = list(map(int, s.split()))
@@ -11,7 +11,7 @@ def solution(s):
 -----------------------------------------------------------------------------------------------------------------------------------------------------
 ## Need to know 
 
-1) string.split(sep, maxsplit): Split a string into a list 
+# 1) string.split(sep, maxsplit): Split a string into a list 
 
 default separator - any whitespace  ex) s.split()
 maxsplit(optional) - specifies how many splits to do. default is -1(all occurances)
@@ -22,14 +22,14 @@ s = "This is me Thank you"
 result = s.split(" " ,1) 
 # result = ['This', 'is me Thank you']
 
-2) list(map(function, iterable)): apply the given function to each item of a given list 
+# 2) list(map(function, iterable)): apply the given function to each item of a given list 
 
 Create new list not changing existing list 
 Commonly used with list and tuple
 ex1)If you want to change datatype of each element of the list: list(map(int, list_name))
-+) map(function, iterable1, iterable2, ...): can be used with multiple iterables
-ex2)
 
++)map(function, iterable1, iterable2, ...): can be used with multiple iterables
+ex2)
 def multiply(a,b):
 	return a * b
  
