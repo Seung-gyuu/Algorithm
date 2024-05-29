ea06314184f7fdc16ad58a8b068a18d55ccdab77
Seunggyu Park <109791533+Seung-gyuu@users.noreply.github.com>
2024-05-29 05:56:53 -0600

    Update 간단한 논리 연산.py

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -5,3 +5,12 @@ def solution(x1, x2, x3, x4):
     y = False if x3 == False and x4 == False else True
     # x ∧ y
     return True if x == True and y == True else False
+    #Another Answer
+    return (x1 | x2 ) & (x3 | x4)
+    # 1. **OR Operation (\(\mid\))**: This operation means that at least one of the conditions on either side of the OR must be true for the whole expression to be true.
+    # - \(x1 \mid x2\) means that either \(x1\) is true, or \(x2\) is true, or both are true.
+    # - \(x3 \mid x4\) means that either \(x3\) is true, or \(x4\) is true, or both are true.
+
+    # 2. **AND Operation (\(\&\))**: This operation means that both conditions on either side of the AND must be true for the whole expression to be true. 
+    #    - \((x1 \mid x2)\) must be true.
+    #    - \((x3 \mid x4)\) must be true.
