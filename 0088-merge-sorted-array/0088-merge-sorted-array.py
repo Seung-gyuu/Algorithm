e2abb8c6c073872466a606dd9083db3e08113104
Seunggyu Park <109791533+Seung-gyuu@users.noreply.github.com>
2025-04-06 03:09:24 -0600

    Time: 0 ms (100%), Space: 17.8 MB (44.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        # Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+        
         #merge nums1 and nums2
         nums1[m:] = nums2
         #sort in ascending order
