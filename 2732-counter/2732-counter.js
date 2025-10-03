d8ee8e117266127aeca136e81dace654fa95d5ed
Seunggyu Park <109791533+Seung-gyuu@users.noreply.github.com>
2025-10-03 12:16:08 -0600

    Time: 35 ms (89.17%), Space: 53.4 MB (72.41%) - LeetHub

--- 2732-counter/2732-counter.js
+++ 2732-counter/2732-counter.js
@@ -3,13 +3,11 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
+    let i = 0;
     return function() {
-        // The post-increment operator (n++) first returns the current value of n, and only after that does it increment the value of n.
-       return n++
+        return n + i++;
     };
 };
-
-// Closures: The inner function remembers the environment (the variables in the outer function's scope), even after the outer function has completed execution. This makes it perfect for creating things like counters, callbacks, or event handlers that maintain their own state.
 
 /** 
  * const counter = createCounter(10)
